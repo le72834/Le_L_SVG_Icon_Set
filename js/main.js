db76d058c49cb67c60722b36fb976c0f4cd96529
Wendy Le <lehong@Les-MacBook-Pro.local>
2020-01-23 20:42:54 -0500

    adding info

--- js/main.js
+++ js/main.js
@@ -4,21 +4,30 @@
 
 (() => {
   console.log("some message - from JavaScript");
-  //find the button element on the page by its ID with JavaScript
-  let allSVGs = document.querySelectorAll(".svg");
+// find the button element on the page by its ID with JavaScript
+  let theHeading = document.querySelector('h1'),
+      //svgImage = document.querySelector("#svgGraphic")
+      allSVGs = document.querySelectorAll(".svg");
 
+  // this is function changes the heading text
+
+  /* function changeText() {
+    theHeading.textContent ="Hello World";
+  }
+  */
   //log the id of the svg to the console when user hovers on it
   function logSVG() {
     console.log(this.id);
   }
 
+
+  //theButton.addEventListener("click", changeText);
+  //svgImage.addEventListener("click", logSVG);
+  //svgImage2.addEventListener("click", logSVG);
   //loop through the collection and apply our event handling to each element
   allSVGs.forEach(item => item.addEventListener("click", logSVG));
 
-
-
-
-
+  //document.querySelector('h1').textContent ="Hello World";
 
 
 
